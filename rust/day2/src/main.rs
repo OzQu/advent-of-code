833bd043481bda1391a8eeb7d29dbf6662faa86b
Oskari Ruutiainen <ozquli@gmail.com>
2022-05-08 19:35:02 +0300

    day2 part1 with rust

---
+++ rust/day2/src/main.rs
@@ -0,0 +1,81 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Direction {
+    Forward(usize),
+    Down(usize),
+    Up(usize)
+}
+
+#[derive(Debug, Default)]
+struct Location {
+    horizontal: usize,
+    depth: usize
+}
+
+// impl Default for Location {
+//     fn default() -> Self {
+//         Location {
+//             horizontal: 0,
+//             depth: 0
+//         }
+//     }
+// }
+
+impl Location {
+    fn move_to(&mut self, directions: &Vec<Direction>) -> () {
+        for direction in directions {
+            match direction {
+                Direction::Forward(units) => self.horizontal = self.horizontal + units,
+                Direction::Down(units) => self.depth = self.depth + units,
+                Direction::Up(units) => self.depth = self.depth - units
+            }
+        }
+    }
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split_whitespace();
+        let direction = split.next();
+        let units = split.next()
+            .unwrap_or("0")
+            .parse::<usize>();
+        let pair = (direction, units);
+        match pair {
+            (Some("forward"), Ok(units)) => Ok(Direction::Forward(units)),
+            (Some("down"), Ok(units)) => Ok(Direction::Down(units)),
+            (Some("up"), Ok(units)) => Ok(Direction::Up(units)),
+            _ => Err(())
+        }
+    }
+    
+}
+
+fn load_from_file(file_path: &str) -> Vec<Direction> {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let directions: Vec<Direction> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<Direction>().unwrap())
+        .collect();
+
+    directions
+}
+
+fn main() {
+    const FILENAME: &str = "day2_input.txt";
+    let directions = load_from_file(FILENAME);
+    println!("{:?}", directions);
+    let mut location = Location::default();
+    println!("original location: {:?}", location);
+    location.move_to(&directions);
+    println!("moved location: {:?}", location);
+    println!("multiply coordinates: {}", location.depth * location.horizontal);
+}
